dfc04d063561cdb26ab8be4794960868f38493b9
TranMachSoHan <50974854+TranMachSoHan@users.noreply.github.com>
2022-04-26 23:49:57 +0700

    COMMENT OUT submission and homework

--- src/main/java/rmit/App.java
+++ src/main/java/rmit/App.java
@@ -1,17 +1,21 @@
 package rmit;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.sql.DataSource;
 
 @SpringBootApplication
-public class App  {
-
+public class App{
+    @Autowired
+    DataSource dataSource;
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args
 
         );
     }
+
 
 }
